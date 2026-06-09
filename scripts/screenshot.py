ba247e14727b8fe5f5177010cd5c7fba82dd0303
Edward G <dev@wallstreet.io>
2026-06-09 14:50:32 -0700

    Make sure .auth exists.

--- scripts/screenshot.py
+++ scripts/screenshot.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
+from enum import Enum
 import os
 from pathlib import Path
 
 import click
 from dotenv import dotenv_values
-from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright
 
 
@@ -16,6 +16,24 @@ class Config:
 
 config_vars = dotenv_values(".env")
 config = Config(**config_vars)
+
+
+class Image(Enum):
+    APP_ACCOUNT_BAR = "app-account-bar.png"
+    CHARTS_APP = "charts-app.png"
+    COMMUNITY_APP = "community-app.png"
+    EDUCATION_APP = "education-app.png"
+
+
+class Url(Enum):
+    BASE = (
+        "https://app.wallstreet.io/chart/AAPL?strategies=discover"
+        "&signal=1&sortColumn=stock&sortDirection=asc&workspace=doji-screener"
+    )
+    LOGIN = "https://app.wallstreet.io/login"
+
+
+CONTEXT = Path("scripts/playwright/.auth/state.json")
 
 
 @click.group()
@@ -56,38 +74,53 @@ def single(url, path, name, full_page, width, height):
         page.screenshot(path=file_path, full_page=full_page)
         browser.close()
 
-    with async_playwright() as p:
-        pass
+    # async with async_playwright() as p:
+    #     pass
 
     size = os.path.getsize(file_path)
     click.echo(f"Saved: {file_path} ({size / 1024:.1f} KB)")
 
 
-def log_in(check_only=False, auth_dir="scripts/playwright/.auth"):
+def login(check_only=False, auth=None, screenshot=False):
     """Login. Save context state."""
-    auth_dir = Path(auth_dir)
+    if auth is None:
+        auth = CONTEXT
     if check_only:
-        state_exists = (auth_dir / "state.json").is_file()
+        state_exists = auth.is_file()
         if state_exists:
-            pass
-    else:
-        with sync_playwright() as p:
-            browser = p.chromium.launch()
-            page = browser.new_page()
+            return
 
-            page.goto("https://app.wallstreet.io/login", wait_until="networkidle")
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        context = browser.new_context()
+        page = context.new_page()
 
-            page.get_by_label("Username or Email").fill(config.USERNAME)
-            page.get_by_role("button", name="Next").click()
-            page.get_by_role("textbox", name="Password").fill(config.PASSWORD)
-            page.get_by_role("button", name="Login").click()
+        page.goto(Url.LOGIN.value, wait_until="networkidle")
 
-            page.locator("a").filter(has_text="Charts").click()
-            # Wait for page to blur to clear.
-            long_cell = page.get_by_role("cell", name="Long").first
-            long_cell.wait_for(state="visible")
-            # page.screenshot(path=file_path, full_page=full_page)
-    # use sync playwright to r
+        page.get_by_label("Username or Email").fill(config.USERNAME)
+        page.get_by_role("button", name="Next").click()
+        page.get_by_role("textbox", name="Password").fill(config.PASSWORD)
+        page.get_by_role("button", name="Login").click()
+
+        page.locator("a").filter(has_text="Charts").click()
+        page.get_by_role("button", name="Doji Screener").click()
+        # Wait for page to blur to clear.
+        # long_cell = page.get_by_role("cell", name="Long").first
+        # long_cell.wait_for(state="visible")
+        storage = context.storage_state(path=auth)
+        if screenshot:
+            page.screenshot(path=Path("screenshots-workflow/fresh") / "login-test.png")
+
+    return storage
+
+
+def screenshot_charts():
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        context = browser.new_context()
+        page = context.new_page()
+        page.get_by_role("button", name="Doji Screener").click()
 
 
 @screenshot.command()
@@ -110,30 +143,33 @@ def log_in(check_only=False, auth_dir="scripts/playwright/.auth"):
 @click.option("--height", "-h", default=720, help="Viewport height (default: 720)")
 def auth_single(url, path, name, full_page, width, height):
     """Take a screenshot of a URL using Playwright."""
-    path = Path(path)
-    file_path = path / f"{name}.png"
+    # path = Path(path)
+    # file_path = path / f"{name}.png"
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page(viewport={"width": width, "height": height})
+    storage = login(screenshot=True)
+    return storage
 
-        click.echo(f"Loading {url}...")
-        page.goto(url, wait_until="networkidle")
+    # with sync_playwright() as p:
+    #     browser = p.chromium.launch()
+    #     page = browser.new_page(viewport={"width": width, "height": height})
 
-        page.get_by_label("Username or Email").fill(config.USERNAME)
-        page.get_by_role("button", name="Next").click()
-        page.get_by_role("textbox", name="Password").fill(config.PASSWORD)
-        page.get_by_role("button", name="Login").click()
+    #     click.echo(f"Loading {url}...")
+    #     page.goto(url, wait_until="networkidle")
 
-        page.locator("a").filter(has_text="Charts").click()
-        # Wait for page to blur to clear.
-        long_cell = page.get_by_role("cell", name="Long").first
-        long_cell.wait_for(state="visible")
-        page.screenshot(path=file_path, full_page=full_page)
-        browser.close()
+    #     page.get_by_label("Username or Email").fill(config.USERNAME)
+    #     page.get_by_role("button", name="Next").click()
+    #     page.get_by_role("textbox", name="Password").fill(config.PASSWORD)
+    #     page.get_by_role("button", name="Login").click()
 
-    size = os.path.getsize(file_path)
-    click.echo(f"Saved: {file_path} ({size / 1024:.1f} KB)")
+    #     page.locator("a").filter(has_text="Charts").click()
+    #     # Wait for page to blur to clear.
+    #     long_cell = page.get_by_role("cell", name="Long").first
+    #     long_cell.wait_for(state="visible")
+    #     page.screenshot(path=file_path, full_page=full_page)
+    #     browser.close()
+
+    # size = os.path.getsize(file_path)
+    # click.echo(f"Saved: {file_path} ({size / 1024:.1f} KB)")
 
 
 if __name__ == "__main__":
