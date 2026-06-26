755465450c522f43e7c479f6631ae8d14234595b
Edward G <dev@wallstreet.io>
2026-06-26 16:31:11 -0700

    Add macros.

--- macros.py
+++ macros.py
@@ -1,4 +1,4 @@
 def define_env(env):
     # Variables are accessible as {{ variable_name }} in templates
     # env.variables["feedback_form_url"] = "https://wallstreet.io"
-    env.variables["feedback_form_url"] = "https://wallstreet.io"
+    env.variables["feedback_form_url"] = "http://localhost"
